a28c8796d530fed9a0229f6cda217c27a80c0e0f
Andrew Chiroque <andreschiroque.07@gmail.com>
2026-08-06 21:21:59 -0500

    Socios and Services

--- proyecto_final/src/main/java/com/cibertec/proyecto_final/entities/ServicioEntity.java
+++ proyecto_final/src/main/java/com/cibertec/proyecto_final/entities/ServicioEntity.java
@@ -1,4 +1,18 @@
 package com.cibertec.proyecto_final.entities;
 
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ServicioEntity {
+    private Long id;
+    private String codigo;
+    private String nombre;
+    private String apellidos;
+    
 }
--- proyecto_final/src/main/java/com/cibertec/proyecto_final/entities/SocioEntity.java
+++ proyecto_final/src/main/java/com/cibertec/proyecto_final/entities/SocioEntity.java
@@ -1,4 +1,20 @@
 package com.cibertec.proyecto_final.entities;
 
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class SocioEntity {
+    private Long id;
+    private String nombre;
+    private String recurrencia;
+    private String moneda;
+    private Double costo;
+    private String destinoCargo;
+    private String tipoCosto;
 }
