32cd878c9e82647432afb980141d88afd80cafd2
escuderoseyner <escudero.seyner@gmail.com>
2026-08-06 22:08:43 -0500

    Completar entidades CuentaCobrar y Giro

--- proyecto_final/src/main/java/com/cibertec/proyecto_final/entities/GiroEntity.java
+++ proyecto_final/src/main/java/com/cibertec/proyecto_final/entities/GiroEntity.java
@@ -1,4 +1,25 @@
 package com.cibertec.proyecto_final.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_giro")
 public class GiroEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_giro")
+    private Integer idGiro;
+
+    @Column(name = "nombre", nullable = false, length = 100)
+    private String nombre;
+
+    @Column(name = "descripcion", length = 255)
+    private String descripcion;
 }
